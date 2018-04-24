953e42d20b6f7c42cd035f226de7b262c17c2a53
lucadolstra <38104438+lucadolstra@users.noreply.github.com>
2018-04-24 12:06:05 +0200

    Initial commit

---
+++ src/main/java/com/capgemini/wardrobe/WardrobeApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.wardrobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WardrobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WardrobeApplication.class, args);
+	}
+}
