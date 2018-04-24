6d6c00d90e4b38a1786339c4fc1628bfeca18466
lucadolstra <38104438+lucadolstra@users.noreply.github.com>
2018-04-24 12:57:19 +0200

    Controller

--- src/main/java/com/capgemini/wardrobe/controller/WardrobeController.java
+++ src/main/java/com/capgemini/wardrobe/controller/WardrobeController.java
@@ -1,4 +1,35 @@
 package com.capgemini.wardrobe.controller;
 
+
+import com.capgemini.wardrobe.object.Wardrobe;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/api/wardrobe/")  //all mappings have this default prefix.
 public class WardrobeController {
+
+    private static ArrayList<Wardrobe> wardrobes = new ArrayList<>();
+
+
+    @GetMapping("{name}")  //allows you to put variables
+    public void createWardrobe(@PathVariable String name) {
+        Wardrobe wardrobe = new Wardrobe(name);
+
+        wardrobes.add(wardrobe);
+    }
+
+
+    //localhost:80808/api/wardrobe/
+    @GetMapping("/")
+    private ArrayList<Wardrobe> getWardrobes() {
+        return wardrobes;
+    }
+
+
+    
 }
